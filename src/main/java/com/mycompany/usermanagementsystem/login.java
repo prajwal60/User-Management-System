68f5bc0920d107679eb51310dd977ddfcce4be1d
Prabesh Guragain <pguragain3@gmail.com>
2020-05-10 22:24:40 +0545

    test cases

--- src/main/java/com/mycompany/usermanagementsystem/login.java
+++ src/main/java/com/mycompany/usermanagementsystem/login.java
@@ -20,7 +20,7 @@ import javax.servlet.http.HttpSession;
  * @author Asus
  */
 @WebServlet(name = "login_1", urlPatterns = {"/login_1"})
-public class login extends HttpServlet {
+public class Login extends HttpServlet {
 
     
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
